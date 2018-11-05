11d66fcf6f1580e621af359e27ca4af09d4d2aa8
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-11-05 20:56:18 +0100

    Add a very simple CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![forbid(unsafe_code)]
 
+extern crate reqwest;
+#[macro_use]
+extern crate structopt;
+
+use structopt::clap::AppSettings;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+name = "git ignore",
+about = "Quickly and easily add templates to .gitignore",
+raw(global_settings = "&[AppSettings::ColoredHelp]")
+)]
+enum Opt {
+    #[structopt(name = "list")]
+    /// List all available .gitignore templates
+    List,
+    #[structopt(name = "get")]
+    /// Get listed templates
+    Get,
+}
+
 fn main() {
-    println!("Hello, world!");
+    match Opt::from_args() {
+        Opt::List => println!("List some shit"),
+        Opt::Get => println!("Get some shit"),
+    }
 }
