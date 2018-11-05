64e9e21b374ba1a0fbf06a0908423810a0c68b64
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-11-05 22:11:30 +0100

    List all matches found
    Though it's fairly crappy, it only matches exact strings

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate reqwest;
 #[macro_use]
 extern crate structopt;
 
+use std::io::Read;
 use structopt::clap::AppSettings;
 use structopt::StructOpt;
 
@@ -18,7 +19,8 @@ enum Opt {
     #[structopt(name = "list")]
     /// List all available .gitignore templates
     List {
-        templates: Option<String>,
+        #[structopt(raw(required = "false"))]
+        templates: Vec<String>,
     },
     #[structopt(name = "get")]
     /// Get listed templates
@@ -27,23 +29,42 @@ enum Opt {
     },
 }
 
-fn gitignore_list(templates: Option<String>) -> Result<(), Box<std::error::Error>> {
+fn gitignore_list(templates: Vec<String>) -> Result<(), Box<std::error::Error>> {
     let url = "https://www.gitignore.io/api/list";
     let mut res = reqwest::get(url)?;
 
+    let all = if templates.is_empty() { true } else { false };
+
     let mut response = Vec::new();
     res.read_to_end(&mut response)?;
-    let mut response = String::from_utf8(response)?;
-    println!("{:?}", response);
+    let response = String::from_utf8(response)?;
+    let response = {
+        let tmp = response.replace("\n", ",");
+        let tmp = tmp.split(",");
+        let mut list: Vec<String> = Vec::new();
+
+        for entry in tmp {
+            if !all {
+                if templates.contains(&entry.to_string()) {
+                    list.push(entry.to_string());
+                }
+            } else {
+                list.push(entry.to_string());
+            }
+        }
+
+        list
+    };
+    println!("{:#?}", response);
+    println!("{:?}", templates);
+    println!("{:?}", all);
 
     Ok(())
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
     match Opt::from_args() {
-        Opt::List { templates } => {
-            gitignore_list(templates)?
-        },
+        Opt::List { templates } => gitignore_list(templates)?,
         Opt::Get { templates } => println!("Get some shit"),
     }
     Ok(())
