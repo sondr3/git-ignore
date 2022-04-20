79b10e51b2b46a73f1c22606bc488358dfa5b324
Sondre Aasemoen <sondre@eons.io>
2022-04-20 11:44:56 +0200

    Add clap_mangen to build man pages, update build.rs

--- build.rs
+++ build.rs
@@ -1,16 +1,53 @@
 use clap::{ArgEnum, IntoApp};
 use clap_complete::generate_to;
-use std::env::current_dir;
+use clap_mangen::Man;
+use std::{
+    env,
+    fs::File,
+    io::Error,
+    path::{Path, PathBuf},
+};
 
 include!("src/cli.rs");
 
-fn main() {
-    let out_path = current_dir().unwrap().join("assets");
-
+fn build_shell_completion(outdir: &Path) -> Result<(), Error> {
     let mut app = CLI::command();
     let shells = Shell::value_variants();
 
     for shell in shells {
-        generate_to(*shell, &mut app, "git-ignore", &out_path).unwrap();
+        generate_to(*shell, &mut app, "git-ignore", &outdir)?;
     }
+
+    Ok(())
+}
+
+fn build_manpages(outdir: &Path) -> Result<(), Error> {
+    let app = CLI::command();
+
+    let file = outdir.join("git-ignore.1");
+    let mut file = File::create(&file)?;
+
+    Man::new(app).render(&mut file)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=src/cli.rs");
+    println!("cargo:rerun-if-changed=man");
+
+    let outdir = match env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(outdir) => outdir,
+    };
+
+    let out_path = PathBuf::from(outdir);
+    let mut path = out_path.ancestors().nth(4).unwrap().to_owned();
+    path.push("assets");
+    std::fs::create_dir_all(&path).unwrap();
+
+    build_shell_completion(&path)?;
+    build_manpages(&path)?;
+
+    Ok(())
 }
